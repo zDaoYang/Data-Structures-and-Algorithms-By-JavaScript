7f0de377103ac4414b7146d95c2a1cca604b580e
zDaoYang <z_daoyang@foxmail.com>
2019-10-22 21:45:46 +0800

    列表改为class es6实现

--- 列表（数组）/列表.js
+++ 列表（数组）/列表.js
@@ -1,100 +1,79 @@
-function List() {
-  this.listSize = 0;
-  this.pos = 0;
-  this.dataStore = []; // 初始化空数组来保存列表元素
-  this.clear = clear;
-  this.find = find; // 找到，返回位对应的index，如果没有，就返回-1
-  this.print = print;
-  this.insert = insert;
-  this.append = append;
-  this.remove = remove;
-  this.end = end;
-  this.prev = prev;
-  this.next = next;
-  this.currPos = getCurrPos; // 返回当前位置
-  this.getElement = getElement;
-  this.contains = contains;
-}
-function clear() {
-  this.listSize = 0;
-  this.dataStore = [];
-}
-function find(element) {
-  for (let i = 0; i < this.dataStore.length; i++) {
-    if (this.dataStore[i] === element) {
-      return i;
+// 列表类
+class List {
+  constructor() {
+    this.listSize = 0; // 列表打消
+    this.pos = 0; // 当前位置
+    this.dataStore = []; // 初始化空数组来保存列表元素
+  }
+
+
+  // 给列表添加元素
+  append(ele) {
+    // 后缀自增
+    this.dataStore[this.listSize++] = ele
+  }
+
+  // 寻找某元素，返回位对应的下标index，如果没有，就返回-1
+  find(ele) {
+    return this.dataStore.indexOf(ele)
+  }
+
+  //从列表中删除元素
+  remove(ele) {
+    const index = this.find(ele)
+    if (index < 0) {
+      throw new Error('没有此元素')
+    }
+    this.dataStore.splice(index, 1)
+    this.listSize--
+  }
+
+  //给定元素和位置，插入该元素
+  insert(index, ele) {
+    if (index < 0) {
+      throw new Error('下标index不能小于0')
+    } else if (index > this.listSize) {
+      throw new Error('下标index不能大于列表长度')
+    } else {
+      this.dataStore.splice(index, 0, ele)
+      this.listSize++
     }
   }
-  return -1;
-}
-function print() { 
-  for(let i = 0; i < this.dataStore.length; i++) {
-    console.log(this.dataStore[i]);
+
+  // 清空列表
+  clear() {
+    this.dataStore = [];
+    this.listSize = 0;
   }
-}
-function insert(loc, ele) {
-  if (loc < 0) {
-    return false;
-  } else {
-    this.dataStore.splice(loc, 0, ele);
-    this.listSize++;
-    return true; 
-  }
-}
-function append(element){
-  this.dataStore[this.listSize++] = element;
-}
-function remove(ele) {
-  let index = this.find(ele);
-  if (index !== -1) {
-    this.dataStore.splice(index, 1);
-    this.listSize--;
-    return true;
-  } else {
-    return false;
-  }
-}
-function end() {
-  return this.dataStore[this.listSize - 1];
-}
-function prev(ele) {
-  let index = this.find(ele);
-  if (index === -1) {
-    return false;
-  } else {
-    return this.dataStore[--index];
-  }
-}
-function next(ele) {
-  let index = this.find(ele);
-  if (index === this.listSize - 1) {
-    return false;
-  } else {
-    return this.dataStore[++index];
-  }
-}
-function getCurrPos(ele) {
-  return this.find(ele);
-}
-function getElement(index) {
-  return this.dataStore[index];
-}
-function contains(ele) {
-  for(let i = 0; i < this.dataStore.length; i++) {
-    if (this.dataStore[i] === ele) {
-      return true;
+
+  //判断给定值是否在列表中
+  contains(ele) {
+    if (this.find(ele) > -1) {
+      return true
+    } else {
+      return false
     }
   }
-  return false;
+
+  //根据下标获取元素
+  getElement(index) {
+    return this.dataStore[index];
+  }
+
+  // 打印出列表
+  print() {
+    console.error(this.dataStore)
+  }
+
 }
-var list1 = new List();
 
-list1.append(12);
-list1.append(123);
-list1.append(1234);
-list1.insert(3, 999);
-list1.remove(123);
-list1.insert(9, 1000);
-list1.print();
-console.log(list1.contains(123));
+var list = new List();
 
+list.append(1);
+list.append(2);
+list.append(3);
+list.insert(3, 999);
+list.insert(2, 888);
+list.remove(2);
+list.print();
+console.log(list.contains(888));
